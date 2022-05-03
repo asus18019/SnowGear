3561fc86923e75e3cd9ab5888ae1e0b6b828835d
asus18019 <72703431+asus18019@users.noreply.github.com>
2022-05-03 18:31:56 +0300

    shop ui

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,6 +6,9 @@ import styles from './navbar.module.css'; // Todo fix import
 const Basket: FC = () => {
 	return (
 		<div className={ styles.navbar }>
+			<Link className={styles.link } to="/">
+				<img className={ styles.logo } src="../assets/logo.png" alt="SnowGear"/>
+			</Link>
 			<Link className={ styles.link } to="/">Main</Link>
 			<Link className={ styles.link } to="/shop">Shop</Link>
 			<Link className={ styles.link } to="/contacts">Contacts</Link>
--- frontend/src/pages/Contacts.tsx
+++ frontend/src/pages/Contacts.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 
 const Contacts: FC = () => {
+	console.log(location);
 	return (
 		<div>
 			Main page
