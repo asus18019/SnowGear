3299270591b19cc118a1e07c575dfa59364b179b
asus18019 <72703431+asus18019@users.noreply.github.com>
2022-06-06 16:44:56 +0300

    pagination for users table

--- frontend/src/components/account/Users.tsx
+++ frontend/src/components/account/Users.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react';
 // @ts-ignore
 import styles from './Users.module.css';
-import { useSortBy, useTable, useGlobalFilter } from 'react-table';
+import { useSortBy, useTable, useGlobalFilter, usePagination } from 'react-table';
 import { useMatch } from 'react-location';
 import { LocationGenerics } from '../../router/accountRouter';
 import GlobalFilter from '../UI/GlobalFilter';
@@ -24,8 +24,8 @@ const Users = () => {
 		hooks.visibleColumns.push((columns: any) => [
 			...columns,
 			{
-				id: "action",
-				Header: "Action",
+				id: 'action',
+				Header: 'Action',
 				// @ts-ignore
 				Cell: ({ row }) => (
 					<p>
@@ -39,11 +39,11 @@ const Users = () => {
 	const ordersData = useMemo(() => [...data], [data]);
 
 	// @ts-ignore
-	const tableInstance = useTable({ columns, data: ordersData }, tableHooks, useGlobalFilter, useSortBy);
+	const tableInstance = useTable({ columns, data: ordersData }, tableHooks, useGlobalFilter, useSortBy, usePagination);
 	// @ts-ignore
-	const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, state, setGlobalFilter } = tableInstance;
+	const { getTableProps, getTableBodyProps, headerGroups, page, nextPage, previousPage, canNextPage, canPreviousPage, pageOptions, gotoPage, pageCount, setPageSize, prepareRow, state, setGlobalFilter } = tableInstance;
 	// @ts-ignore
-	const { globalFilter } = state;
+	const { globalFilter, pageIndex, pageSize } = state;
 
 	return (
 		<div className={ styles.users__wrapper }>
@@ -52,9 +52,9 @@ const Users = () => {
 			<GlobalFilter filter={ globalFilter } setFilter={ setGlobalFilter } />
 			<table { ...getTableProps() } className={ styles.users_table }>
 				<thead className={ styles.users_table__thead }>
-					{ headerGroups.map((headerGroup) => (
+					{ headerGroups.map(headerGroup => (
 						<tr { ...headerGroup.getHeaderGroupProps() }>
-							{ headerGroup.headers.map((column) => (
+							{ headerGroup.headers.map(column => (
 								// @ts-ignore
 								<th { ...column.getHeaderProps(column.getSortByToggleProps()) }
 								    className={ styles.users_table__th }
@@ -71,12 +71,12 @@ const Users = () => {
 				</thead>
 				<tbody { ...getTableBodyProps() } className={ styles.users_table__tbody }>
 					{
-						rows.map(row => {
+						page.map((row: any) => {
 							prepareRow(row);
 
 							return <tr { ...row.getRowProps() } className={ styles.users_table__tr }>
 								{
-									row.cells.map(cell => (
+									row.cells.map((cell: any) => (
 										<td
 											className={ styles.users_table__td }
 											key={ cell.column.id }
@@ -92,6 +92,37 @@ const Users = () => {
 					}
 				</tbody>
 			</table>
+			<div className={ styles.pagination__wrapper }>
+				<div className={ styles.pagination__info }>
+					<span>Page <strong>{ pageIndex + 1 } of {pageOptions.length} </strong></span>
+					<span>| Go to page: <input
+						className={ styles.pagination__goto_input }
+						type="number"
+						defaultValue={pageIndex + 1}
+						onChange={e => {
+							const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0;
+							gotoPage(pageNumber);
+						}}
+						style={{ width: '50px' }}
+					/>
+					</span>
+					<select className={ styles.pagination__size_select }
+						value={pageSize}
+						onChange={e => setPageSize(Number(e.target.value))}>
+						{[5, 10, 25, 50].map(pageSize => (
+							<option key={pageSize} value={pageSize}>
+								Show {pageSize}
+							</option>
+						))}
+					</select>
+				</div>
+				<div className={ styles.pagination__buttons }>
+					<button className={ styles.pagination__button } onClick={ () => gotoPage(0) } disabled={ !canPreviousPage }>{'<<'}</button>
+					<button className={ styles.pagination__button } onClick={ () => previousPage() } disabled={ !canPreviousPage }>Previous</button>
+					<button className={ styles.pagination__button } onClick={ () => nextPage() } disabled={ !canNextPage }>Next</button>
+					<button className={ styles.pagination__button } onClick={ () => gotoPage(pageCount - 1) } disabled={ !canNextPage }>{'>>'}</button>
+				</div>
+			</div>
 		</div>
 	);
 };
